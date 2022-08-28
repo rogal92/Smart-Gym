604b9c5bf39e7b832d4c7c31a43b752c293a6e08
Bartosz Rogalski <rogalski92@hotmail.com>
2022-08-28 10:01:55 +0200

    db refactor

--- app/src/main/java/com/example/smartgym/database/GymDatabase.java
+++ app/src/main/java/com/example/smartgym/database/GymDatabase.java
@@ -1,13 +1,12 @@
 package com.example.smartgym.database;
 
-import static android.database.sqlite.SQLiteDatabase.CursorFactory;
+import static com.example.smartgym.database.GymDatabaseProvider.*;
 
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 import com.example.smartgym.dao.Coach;
@@ -24,72 +23,26 @@ public class GymDatabase {
     public static final int NAME_COLUMN = 1;
     public static final int COACHES_COLUMN = 2;
     public static final int EQUIPMENT_COLUMN = 3;
-    private static final int DB_VERSION = 1;
-    private static final String DEBUG_TAG = "GYM DATABASE";
-    private static final String DB_NAME = "database.db";
-    private static final String GYM_TABLE = "GYM";
-    private static final String COACH_TABLE = "COACH";
-    private static final String EQUIPMENT_TABLE = "EQUIPMENT";
-    private static final String DB_CREATE_COACH_TABLE = """
-            CREATE TABLE COACH (
-            ID INTEGER PRIMARY KEY AUTOINCREMENT,
-            NAME TEXT NOT NULL);
-            """;
-    private static final String DB_CREATE_EQUIPMENT_TABLE = """
-            CREATE TABLE EQUIPMENT (
-            ID INTEGER PRIMARY KEY AUTOINCREMENT,
-            NAME TEXT NOT NULL,
-            DESCRIPTION TEST NOT NULL);
-            """;
-    private static final String DB_CREATE_GYM_TABLE = """
-            CREATE TABLE GYM (
-            ID INTEGER PRIMARY KEY AUTOINCREMENT,
-            NAME TEXT NOT NULL,
-            LOCATION TEXT NOT NULL);
-            """;
     private SQLiteDatabase db;
     private Context context;
-    private DatabaseService dbService;
+    private GymDatabaseProvider dbCreator;
 
     public GymDatabase(Context context) {
         this.context = context;
     }
 
     public GymDatabase open() {
-        dbService = new DatabaseService(context, DB_NAME, null, DB_VERSION);
+        dbCreator = new GymDatabaseProvider(context, DB_NAME, null, DB_VERSION);
         try {
-            db = dbService.getWritableDatabase();
+            db = dbCreator.getWritableDatabase();
         } catch (SQLException e) {
-            db = dbService.getReadableDatabase();
+            db = dbCreator.getReadableDatabase();
         }
         return this;
     }
 
     public void close() {
-        dbService.close();
-    }
-
-    private static class DatabaseService extends SQLiteOpenHelper {
-
-        public DatabaseService(Context context, String name, CursorFactory factory, int version) {
-            super(context, name, factory, version);
-        }
-
-        @Override
-        public void onCreate(SQLiteDatabase db) {
-            Log.d(DEBUG_TAG, "Creating database...");
-            db.execSQL(DB_CREATE_GYM_TABLE);
-            Log.d(DEBUG_TAG, "Table " + GYM_TABLE + " ver." + DB_VERSION + " created");
-            db.execSQL(DB_CREATE_COACH_TABLE);
-            Log.d(DEBUG_TAG, "Table " + COACH_TABLE + " ver." + DB_VERSION + " created");
-            db.execSQL(DB_CREATE_EQUIPMENT_TABLE);
-            Log.d(DEBUG_TAG, "Table " + EQUIPMENT_TABLE + " ver." + DB_VERSION + " created");
-        }
-
-        @Override
-        public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
-            //moving old version of db on the device to the new one after update
-        }
+        dbCreator.close();
     }
 
     /*TODO
---
+++ app/src/main/java/com/example/smartgym/database/GymDatabaseProvider.java
@@ -0,0 +1,55 @@
+package com.example.smartgym.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+class GymDatabaseProvider extends SQLiteOpenHelper {
+    static final int DB_VERSION = 1;
+    static final String DEBUG_TAG = "GYM DATABASE";
+    static final String DB_NAME = "database.db";
+    static final String GYM_TABLE = "GYM";
+    static final String COACH_TABLE = "COACH";
+    static final String EQUIPMENT_TABLE = "EQUIPMENT";
+    private static final String DB_CREATE_COACH_TABLE = """
+            CREATE TABLE COACH (
+            _ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            LOCATION TEXT NOT NULL,
+            NAME TEXT NOT NULL);
+            """;
+    private static final String DB_CREATE_EQUIPMENT_TABLE = """
+            CREATE TABLE EQUIPMENT (
+            _ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            NAME TEXT NOT NULL,
+            DESCRIPTION TEST NOT NULL);
+            """;
+    private static final String DB_CREATE_GYM_TABLE = """
+            CREATE TABLE GYM (
+            _ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            NAME TEXT NOT NULL,
+            LOCATION TEXT NOT NULL);
+            """;
+
+    public GymDatabaseProvider(Context context, String name, SQLiteDatabase.CursorFactory factory,
+                               int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        Log.d(DEBUG_TAG, "Creating database...");
+        db.execSQL(DB_CREATE_GYM_TABLE);
+        Log.d(DEBUG_TAG, "Table " + GYM_TABLE + " ver." + DB_VERSION + " created");
+        db.execSQL(DB_CREATE_COACH_TABLE);
+        Log.d(DEBUG_TAG, "Table " + COACH_TABLE + " ver." + DB_VERSION + " created");
+        db.execSQL(DB_CREATE_EQUIPMENT_TABLE);
+        Log.d(DEBUG_TAG, "Table " + EQUIPMENT_TABLE + " ver." + DB_VERSION + " created");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+//        db.execSQL("DROP TABLE IF EXISTS " + GYM_TABLE);
+//        onCreate(db);
+    }
+}
