84502fd9215c6f90acae5e718689d786bce1cada
Bartosz Rogalski <rogalski92@hotmail.com>
2022-09-10 12:48:06 +0200

    added images to equipment

--- app/src/main/java/com/example/smartgym/constants/EquipmentImage.java
+++ app/src/main/java/com/example/smartgym/constants/EquipmentImage.java
@@ -3,7 +3,11 @@ package com.example.smartgym.constants;
 import com.example.smartgym.R;
 
 public enum EquipmentImage {
-    AB_ROLLER(R.drawable.abroller);
+    AB_ROLLER(R.drawable.abroller),
+    BENCH_PRESS(R.drawable.benchpress),
+    CHEST_FLY_MACHINE(R.drawable.chestflymachine),
+    CHEST_PRESS_MACHINE(R.drawable.chestpressmachine),
+    DECLINED_BENCH_PRESS(R.drawable.declinedbenchpress);
 
     private int resource;
 
